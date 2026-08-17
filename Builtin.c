9ee2c7dc35e560371c29445b13d84be7438a4dbd
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-17 14:02:34 +0200

    Create builtin.c

---
+++ Builtin.c
@@ -0,0 +1,25 @@
+#include "simpleshell.h"
+
+/**
+ * handle_builtin - Vérifie et exécute un builtin
+ * @argv: tableau d'arguments
+ *
+ * Return: 1 si un builtin a été exécuté, 0 sinon
+ */
+int handle_builtin(char **argv)
+{
+    /* builtin: exit */
+    if (strcmp(argv[0], "exit") == 0)
+    {
+        exit(0);
+    }
+
+    /* builtin: env */
+    if (strcmp(argv[0], "env") == 0)
+    {
+        print_environment(environ);
+        return (1);
+    }
+
+    return (0);
+}
